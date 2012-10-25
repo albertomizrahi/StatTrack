50b201b6ad4e936a08b8e814cfa6b60a51d59689
Alberto Mizrahi <amb110395@gmail.com>
2012-10-24 21:32:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatHub::Application.config.secret_token = 'df153fe70b2eea6f350ceaff696e4e35239bf301c3de9bf49b49c64f18c3d3159fbe160e6867c7390fff5795696b2c8cce94a3b4c36e9f18039862aecd3ef74a'
