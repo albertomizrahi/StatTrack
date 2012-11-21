e7e706e198def51fc19100b04e97f46eb3cc1d4e
Alberto Mizrahi <amb110395@gmail.com>
2012-11-21 18:52:46 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+      #Flashes a message welcoming the new user
+      flash[:success] = "Welcome to the Sample App!"
+      # Afterwards, it redirects the user to its respective profile page
+      redirect_to @user
+    else
+      #After the new view is rendered, the errors will be displayed in the error_messages partial
+      render 'new'
+    end
+  end
+
 end
