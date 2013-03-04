c739d93df9de179e08ea38e7252d59ddd6a3e68f
Alberto Mizrahi <amb110395@gmail.com>
2013-03-03 21:31:48 -0500

    Change seeds file to add sports to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+["Basketball", "Football"].each do |sport|
+  Sport.find_or_create_by_name sport
+end
